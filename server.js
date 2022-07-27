6fb51a2f3f905a995502075e825d0738336ac96a
yaser01 <yasser.milato@gmail.com>
2022-07-28 02:48:59 +0300

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/quizz-react'));
+app.use(express.static(__dirname + '/build'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/quizz-react/index.html'));});
+'/build/index.html'));});
 app.listen(process.env.PORT || 8080);
