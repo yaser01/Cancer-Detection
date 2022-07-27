15dd99748df72de7ef3b41549519c0a3cc629ab4
yaser01 <yasser.milato@gmail.com>
2022-07-28 02:54:43 +0300

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 const app = express();
-app.use(express.static(__dirname + '/build'));
+app.use(static(__dirname + '/build'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
+res.sendFile(join(__dirname+
 '/build/index.html'));});
 app.listen(process.env.PORT || 8080);
