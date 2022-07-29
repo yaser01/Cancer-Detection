c05cd42bc6f4fc16044f26ee218eddffcd8b8aa5
yaser01 <yasser.milato@gmail.com>
2022-07-29 04:02:15 +0300

    update

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const path = require('path')
 const port = process.env.PORT || 3001
 
 app.use('/', express.static(path.join(__dirname, 'build')))
-app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../Y/build', 'index.html');
-    if (!url.startsWith('/app/')) // since we're on local windows
-      url = url.substring(1);
-    res.sendFile(url);
-  });
+
+app.listen(port, () => console.log("Listening on Port", port)) 
 
